892eef67d4e56abdd289ffb8651aeefc1027c157
chenshenhai <cshenhai@gmail.com>
2022-01-19 22:26:29 +0800

    feat: init repo

---
+++ scripts/dev.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const { createServer, defineConfig } = require('vite');
+const reactRefresh = require('@vitejs/plugin-react-refresh');
+const open = require('open');
+
+dev();
+
+async function dev() {
+  const viteConfig = getViteConfig();
+  const server = await createServer(viteConfig)
+  await server.listen()
+  server.printUrls();
+  const { port, host = '127.0.0.1' } = server.config?.server || {}
+  const targetPage = `http://${host}:${port}/index.html`;
+  console.log(
+    `Open: ` + targetPage
+  );
+  await open(targetPage);
+}
+
+function getViteConfig() {
+  const viteConfig = defineConfig({
+    configFile: false,
+    root: resolve(),
+    publicDir: resolve('example'),
+    server: {
+      port: 8080,
+      host: '127.0.0.1',
+    },
+    plugins: [reactRefresh()],
+    esbuild: {
+      include: [
+        /\.ts$/,
+        /\.tsx$/,
+        /\.js$/,
+        /\.jsx$/,
+      ],
+      exclude: [
+        /\.html$/
+      ]
+    },
+  });
+  return viteConfig;
+}
+
+function resolve(...args) {
+  return path.join(__dirname, '..', ...args);
+}
---
+++ src/demo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './index';
+import './css';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#app')
+)
+
