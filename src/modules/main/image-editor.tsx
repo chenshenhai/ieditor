89fb42acff422706ed917fc2386e71b90da471ae
chenshenhai <cshenhai@gmail.com>
2022-02-02 12:38:08 +0800

    feat: update module main

--- src/modules/main/image-editor.tsx
+++ src/modules/main/image-editor.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { getCls } from './common';
+import { Context } from '../../context';
 
 export function ImageEditor() {
+
+  const { store } = useContext(Context);
+  const { currentWebFile } = store;
+  let src = '';
+  if (typeof currentWebFile.content === 'string' && currentWebFile.content) {
+    src = currentWebFile.content;
+  }
+
   return (
     <div className={getCls('image-editor')}>
-      Image xxxx
+      <img className={getCls('image')} src={src} />
     </div>
   )
 }
