b371ab3320c3d97aa9565c4ffbfd8b6ada6480ce
chenshenhai <cshenhai@gmail.com>
2022-02-12 18:48:09 +0800

    chore: update demo

--- example/demo.tsx
+++ example/demo.tsx
@@ -21,7 +21,6 @@ const value = `
   - React
   - Ant Design
 
-
 ![nodejs](${imageNodejsBase64})
 
 
@@ -31,9 +30,139 @@ const value = `
 
 The file system is based on [File System Access API](https://developer.mozilla.org/en-US/docs/Web/API/File_System_Access_API)
 
-## Content
+## Markdown Syntax
+
+### Headings
+
+Add number signs(#) in front of a word or phrase.
+
+# Heading level 1
+## Heading level 2
+### Heading level 3
+#### Heading level 4
+##### Heading level 5
+###### Heading level 6
+
+### Paragraphs
+
+Use a blank line to seperate one or more lines of text.
+
+### Emphasis
+
+#### Bold
+
+Add two asterisks or underscores before and after a word or phrase.
+
+This is  **Bold Text** by asterisks.
+
+This is  __Bold Text__ by underscores.
+
+#### Italic
+
+Add one asterisk or underscore before and after a word or phrase
+
+This is  *Italic Text*  by asterisks.
+
+This is  _Italic Text_  by underscore.
+
+### Blockquotes
+
+Add a > in front of a paragraph.
+
+> This is Blockquotes Paragraph
+>
+> > This is Blockquotes Paragraph
+
+### Lists
+
+
+#### Order Lists
+
+Add line items with numbers followed by periods.
+
+1. Item 001
+1. Item 002
+1. Item 003
+1. Item 004
+1. Item 005
+
+<br>
+
+1. Item 001
+2. Item 002
+3. Item 003
+4. Item 004
+5. Item 005
+
+#### Unordered Lists
+
+Add dashes (-), asterisks (*), or plus signs (+) in front of line items.
+
+##### Unordered Lists By Dashes
+
+- Item 001 by dashes
+  - Item 002 by dashes
+  - Item 003 by dashes
+- Item 004 by dashes
+  - Item 005 by dashes
+  - Item 006 by dashes
+
+##### Unordered Lists By Asterisks
+
+* Item 001 by asterisks
+  * Item 002 by asterisks
+  * Item 003 by asterisks
+* Item 004 by asterisks
+  * Item 005 by asterisks
+  * Item 006 by asterisks
+
+
+##### Unordered Lists By Signs
+
++ Item 001 by signs
+  + Item 002 by signs
+  + Item 003 by signs
++ Item 004 by signs
+  + Item 005 by signs
+  + Item 006 by signs
+
+### Links
+
+Enclose the link text in brackets (e.g., [Hello World]) and then follow it immediately with the URL in parentheses (e.g., (https://github.com)).
+
+This is  [Github Link](https://github.com)
+
+
+#### Add Links Title
+
+This is  [Github Link](https://github.com "Hello GitHub")
+
+
+### Images
+
+Add an exclamation mark (\\!), followed by alt text in brackets, and the path or URL to the image asset in parentheses. You can optionally add a title in quotation marks after the path or URL.
+
+![Hello Gitee](https://gitee.com/static/images/logo-black.svg?t=158106664)
+
+### Escaping Characters
+
+To display a literal character that would otherwise be used to format text in a Markdown document, add a backslash (\\) in front of the character.
+
+\\*Hello\\*
+  
 
 ### Code
+
+#### Escaping Backticks
+
+Enclosing the code in double backticks (\`\`).
+
+\`React\`, \`React-DOM\`
+
+
+#### Code Blocks
+
+Indent every line of the block by at least four spaces or one tab.
 
 \`\`\`js
 import React from 'react';
@@ -48,6 +177,11 @@ ReactDOM.render(<App />, document.querySelector('#app'))
 
 
 
+### HTML
+
+Place the tags in the text of your Markdown-formatted file.
+
+ Hello <span style="color:red; font-weight:bolder;">World</span>
 
 `;
 
